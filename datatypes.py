d933ee7a576542581358f25bfa25e05fdb84a45b
krishhnamohanmishra <krishnateach716@gmail.com>
2026-01-13 19:11:58 +0530

    data type notes upload

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+name="krishna"
+age=23
+height=5.9
+is_student=True
+is_employed=False
+is_armyofficer=None
+print("name type:", type(name))
+print("age type:", type(age))
+print("height type:", type(height))
+print("is_student type:", type(is_student))
+print("is_employed type:", type(is_employed))
+print("is_armyofficer type:", type(is_armyofficer))
