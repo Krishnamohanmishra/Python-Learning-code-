9b2a828f79e22312d5bb3ec092cb3e9636610a6c
krishhnamohanmishra <krishnateach716@gmail.com>
2026-01-14 20:54:35 +0530

    relational operator

--- operator.py
+++ operator.py
@@ -2,10 +2,22 @@
 #( + , - , * , / , % , ** , // )  
 a=2
 b=2
-print("a + b = " ,a+b) 
-print("a - b = " ,a-b) 
-print("a * b = " ,a*b) 
-print("a / b = " ,a/b) 
-print("a % b = " ,a%b) 
-print("a ** b = " ,a**b) 
-print("a // b = " ,a//b) 
+print("a + b = " ,a+b) #additional
+print("a - b = " ,a-b) #subtraction 
+print("a * b = " ,a*b) #multiplication
+print("a / b = " ,a/b) #division
+print("a % b = " ,a%b) #modulus
+print("a ** b = " ,a**b) #exponentiation 
+print("a // b = " ,a//b) #floor division (interger value)
+
+#comprison / Relational Operators 
+#(== , != , > , < , >= , <= )
+
+a=33
+b=3
+print("a == b = " ,a==b) #equal to 
+print("a != b = " ,a!=b) #not equal to
+print("a > b = " ,a>b)   #greater than 
+print("a < b = " ,a<b)   #less than 
+print("a >= b = " ,a>=b) #greater than or equal to
+print("a <= b = " ,a<=b) #less than or equal to 
